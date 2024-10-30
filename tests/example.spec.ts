b9b5a7de9beb4ecae6c5363c392dfdb0662b1ec1
Yaroslav Larin <cyberbiont@gmail.com>
2024-10-30 13:28:58 +0200

    initial

---
+++ tests/example.spec.ts
@@ -0,0 +1,29 @@
+import { expect, test } from '@playwright/test';
+
+test.only(
+	'has title @test2',
+	{
+		tag: '@test',
+	},
+	async ({ page }) => {
+		await page.goto('https://playwright.dev/');
+
+		// Expect a title "to contain" a substring.
+		await expect(page).toHaveTitle(/Playwright/);
+
+		expect(page.getByRole('link', { name: 'Getting started' }))
+			.toBeDefined;
+	}
+);
+
+test('get started link', async ({ page }) => {
+	await page.goto('https://playwright.dev/');
+
+	// Click the get started link.
+	await page.getByRole('link', { name: 'Get started' }).click();
+
+	// Expects page to have a heading with the name of Installation.
+	await expect(
+		page.getByRole('heading', { name: 'Installation' })
+	).toBeVisible();
+});
